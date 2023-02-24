8318fdf9752d5ad45cbc18a693ed608104c5162d
shelby <shelbynarnold@yahoo.com>
2023-02-24 11:02:42 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(Fenris){
+    return cats.push(Fenris)
+}
+
+function destructivelyPrependCat(Boots){
+    return cats.unshift(Boots)
+}
+
+function destructivelyRemoveLastCat(Garfield){
+    return cats.pop(Garfield)
+}
+
+function destructivelyRemoveFirstCat(Milo){
+    return cats.shift(Milo)
+}
+
+function appendCat(Broom){
+    return [...cats, Broom]
+}
+
+function prependCat(Broom){
+    return [Broom, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, 2)
+}
+
+function removeFirstCat(){
+    return cats.slice(1, 3)
+}
